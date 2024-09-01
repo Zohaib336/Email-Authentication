f8df06a8137e704297444eb481581b16e6b44e96
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-01 13:19:12 +0530

    signup endpoint

---
+++ server/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "5d",
+    });
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+    });
+
+    return token;
+};
