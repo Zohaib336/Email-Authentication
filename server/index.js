9f81ff4a7a575439d5c5501d06caf65afedad83c
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-04 23:41:47 +0530

    check auth endpoint

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './db/connectDB.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
