f434922ea9227a41b202e116db9725548da50b7f
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-05 23:09:23 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-green-500 text-5xl grid place-items-center h-screen">Email Verification</h1>
+    </>
+  )
+}
+
+export default App
